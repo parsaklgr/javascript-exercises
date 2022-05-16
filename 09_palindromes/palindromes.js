4c087567fd7c403070f5636d8efe79059487bc5d
parsaklgr <parsaklgr@gmail.com>
2022-05-17 00:00:42 +0430

    Finish to exercise 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const regex = /\w/;
+    let stringArr = str.toLowerCase().split("").filter(char => regex.test(char));
+    for (let i = 0; i < stringArr.length; i++) {
+        if (stringArr[i] !== stringArr[stringArr.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    if (typeof index === "string") {
+        index = parseInt(index);
+    }
+    if (index < 1) {
+        return "OOPS";
+    }
+    let firstNum = 1;
+    let secondNum = 1;
+    for (let i = 2; i < index; i++){
+        let temp = firstNum;
+        firstNum = secondNum;
+        secondNum = temp + secondNum;
+    }
+    return secondNum;
 };
 
 // Do not edit below this line
