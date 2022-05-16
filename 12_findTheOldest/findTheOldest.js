b58880a1ec800d0cb505b07fd1fcfb00f88bb7ad
parsaklgr <parsaklgr@gmail.com>
2022-05-16 23:58:59 +0430

    Finish to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (Object.keys(oldest).length === 0) {
+            return person;
+        }
+        let oldestBirth = oldest["yearOfBirth"];
+        let oldestAge = 0;
+        if ("yearOfDeath" in oldest) {
+            oldestAge = oldest["yearOfDeath"] - oldestBirth;
+        } else {
+            let d = new Date();
+            oldestAge = d.getUTCFullYear() - oldestBirth;
+        }
+        let personBirth = person["yearOfBirth"];
+        let personAge = 0;
+        if ("yearOfDeath" in person) {
+            personAge = person["yearOfDeath"] - personBirth;
+        } else {
+            let d = new Date();
+            personAge = d.getUTCFullYear() - personBirth;
+        }
+        if (personAge > oldestAge) {
+            return person;
+        } else {
+            return oldest;
+        }
+    }, {});
 };
 
 // Do not edit below this line
